1afe676e23c1ec034742b4da4399d16ca5e37095
Thomas Balthazar <thomas@balthazar.info>
2017-01-03 17:24:02 +0100

    Add more folders to the oimap quick script

--- config-files/.bin/oimap-quick.sh
+++ config-files/.bin/oimap-quick.sh
@@ -3,6 +3,9 @@
 folders="INBOX"
 folders="$folders,INBOX.Rules.Lists.Neutrinet.Cube"
 folders="$folders,INBOX.Rules.Lists.Neutrinet.Tech"
+folders="$folders,INBOX.Rules.Lists.Neutrinet.Admin"
+folders="$folders,INBOX.Rules.Lists.Neutrinet.General"
+folders="$folders,INBOX.Rules.GitHub"
 
 /usr/local/bin/offlineimap -o \
                            -a Fastmail \
