4e83c7bfeb336911e263b88c0458208813b419a1
Thomas Balthazar <thomas@balthazar.info>
2017-01-18 14:42:30 +0100

    Make sure oimap-quick sync folders with space

--- config-files/.bin/oimap-quick.sh
+++ config-files/.bin/oimap-quick.sh
@@ -8,7 +8,9 @@ folders="$folders,INBOX.Rules.Lists.Neutrinet.Admin"
 folders="$folders,INBOX.Rules.Lists.Neutrinet.General"
 folders="$folders,INBOX.Rules.GitHub"
 
+echo $folders
+
 /usr/local/bin/offlineimap -o \
                            -a Fastmail \
-                           -f $folders \
+                           -f '$folders' \
                            -u quiet
