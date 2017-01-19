d85b0af752a0e9faa8a7b3ac39e1665eb3486adc
Thomas Balthazar <thomas@balthazar.info>
2017-01-19 13:24:47 +0100

    Another fix for oimap-quick

--- config-files/.bin/oimap-quick.sh
+++ config-files/.bin/oimap-quick.sh
@@ -8,9 +8,7 @@ folders="$folders,INBOX.Rules.Lists.Neutrinet.Admin"
 folders="$folders,INBOX.Rules.Lists.Neutrinet.General"
 folders="$folders,INBOX.Rules.GitHub"
 
-echo $folders
-
 /usr/local/bin/offlineimap -o \
                            -a Fastmail \
-                           -f '$folders' \
+                           -f "$folders" \
                            -u quiet
