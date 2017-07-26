7a24c6195a70169adfdc925c473f83b637950ccc
Thomas Balthazar <thomas@balthazar.info>
2017-07-26 12:17:31 +0200

    Update offlineimap quick script

--- config-files/.bin/oimap-quick.sh
+++ config-files/.bin/oimap-quick.sh
@@ -2,11 +2,6 @@
 
 folders="INBOX"
 folders="$folders,INBOX.Sent Items"
-folders="$folders,INBOX.Rules.Lists.Neutrinet.Cube"
-folders="$folders,INBOX.Rules.Lists.Neutrinet.Tech"
-folders="$folders,INBOX.Rules.Lists.Neutrinet.Admin"
-folders="$folders,INBOX.Rules.Lists.Neutrinet.General"
-folders="$folders,INBOX.Rules.GitHub"
 
 /usr/local/bin/offlineimap -o \
                            -a Fastmail \
