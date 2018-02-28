852c9a7f5963994832232024df5d61780266ca22
Thomas Balthazar <thomas@balthazar.info>
2018-02-28 19:56:10 +0100

    Fix offlineimap config

--- config-files/.offlineimap.py
+++ config-files/.offlineimap.py
@@ -2,8 +2,8 @@
 from subprocess import check_output
 
 def get_property(path, account, property):
-    # gpg2 -dq ~/.email-pwds.gpg | grep my_email_fastmail | awk '{print $4}' | tr -d '"'
-    cmd = "gpg2 -dq " + path + " | grep my_" + property + "_" + account + " | awk '{print $4}' | tr -d '\"'"
+    # gpg -dq ~/.email-config.gpg | grep my_email_fastmail | awk '{print $4}' | tr -d '"'
+    cmd = "gpg -dq " + path + " | grep my_" + property + "_" + account + " | awk '{print $4}' | tr -d '\"'"
     return check_output(cmd, shell=True).strip("\n")
 
-# print(get_property("~/.email-pwds.gpg", "gandi", "email"))
+# print(get_property("~/.email-config.gpg", "gandi", "email"))
