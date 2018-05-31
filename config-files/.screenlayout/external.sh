0d62676a7ec36596fad6fd8efa30ccc3780bbcd7
Thomas Balthazar <thomas@balthazar.info>
2018-05-31 23:18:56 +0200

    Add screen layout scripts

---
+++ config-files/.screenlayout/external.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off --dpi 180
