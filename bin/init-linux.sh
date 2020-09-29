40784f703a4f11d265f62c0c14ff49d100eb61ee
Thomas Balthazar <thomas@balthazar.info>
2020-09-29 19:06:42 +0200

    Add basic init script for Linux

---
+++ bin/init-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Install rcm
+wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt-get update
+sudo apt-get install rcm
+
+printf "\n\n-- rcm installed\n\n"
+
+# Install zsh
+sudo apt-get update && sudo apt-get -y install zsh
+chsh -s /bin/zsh
+
+printf "\n\n-- zsh installed. Please log out and log back in.\n\n"
