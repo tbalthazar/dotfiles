086925ad78141848c1f7b054c68f878189291f67
Thomas Balthazar <thomas@balthazar.info>
2020-10-02 15:05:58 +0200

    Tweak linux init script
    Install a few default packages and tmux theme

--- bin/init-linux.sh
+++ bin/init-linux.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+# Install base packages
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y \
+  adduser \
+  curl \
+  git \
+  gnupg2 \
+  htop \
+  tmux \
+  vim-gtk \
+  vim-nox \
+  wget \
+  xclip \
+  --no-install-recommends
+
+printf "\n\n-- base package installed\n\n"
+
 # Install rcm
 wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
 echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
@@ -11,9 +29,15 @@ sudo apt-get install rcm
 printf "\n\n-- rcm installed\n\n"
 
 # Init rcm
-rcup -d $HOME/.dotfiles/dotfiles -t linux
+rcup -v -d $HOME/.dotfiles/dotfiles -t linux
 
-printf "\n\n-- rcm configured\n\n"
+printf "\n\n-- dotfiles installed\n\n"
+
+# Tmux theme
+mkdir -p $HOME/.tmux/themes
+git clone https://github.com/arcticicestudio/nord-tmux $HOME/.tmux/themes/nord-tmux
+
+printf "\n\n-- tmux theme installed\n\n"
 
 # Install zsh
 sudo apt-get update && sudo apt-get -y install zsh
