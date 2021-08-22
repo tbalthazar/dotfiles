dc8cb01c2f8a5b244e59d843635e86e81264ea33
Thomas Balthazar <thomas@balthazar.info>
2021-08-22 12:18:45 +0200

    neovim: automatic goimports

--- dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -35,6 +35,42 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
     vim.api.nvim_command [[augroup END]]
   end
+
+  local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
+
+  if filetype == 'go' then
+    vim.cmd [[autocmd BufWritePre <buffer> :lua goimports(2000)]]
+  end
+end
+
+function goimports(timeout_ms)
+  local context = { only = { "source.organizeImports" } }
+  vim.validate { context = { context, "t", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/handler.lua) for how to do this properly.
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result or next(result) == nil then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
+
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      vim.lsp.util.apply_workspace_edit(action.edit)
+    end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
+  end
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
