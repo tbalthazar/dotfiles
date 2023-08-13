636d26e39d9ce053b499bef321d8f3bcf5abae6e
Thomas Balthazar <thomas@balthazar.info>
2023-08-13 17:48:59 +0200

    neovim: add binding to delete buffer

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -299,7 +299,7 @@ require('telescope').setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
       },
     },
   },
