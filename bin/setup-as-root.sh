97332f131bd3f28c9270c4776e291d6bb92b484b
Thomas Balthazar <thomas@balthazar.info>
2023-08-13 17:56:35 +0200

    Minor tweak to install script

--- bin/setup-as-root.sh
+++ bin/setup-as-root.sh
@@ -58,8 +58,7 @@ apt-get install -y \
 echo "[+] Installing 1Password..."
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | \
-  tee /etc/apt/sources.list.d/1password.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' > /etc/apt/sources.list.d/1password.list
 mkdir -p /etc/debsig/policies/AC2D62742012EA22/
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
   tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
