84e5fb4fdac34705a676c2e1f6c13185efffa16b
Thomas Balthazar <thomas@balthazar.info>
2023-08-13 17:47:18 +0200

    neovim: customize lualine

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -152,6 +152,22 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1
+          }
+        },
+      },
+      inactive_sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1
+          }
+        },
+      },
     },
   },
 
