41773f7dc2ca60b8d93bab85bc575aa0d61ebe82
Thomas Balthazar <thomas@balthazar.info>
2023-08-29 19:35:39 +0200

    cleanup

--- bin/setup-as-tb.sh
+++ bin/setup-as-tb.sh
@@ -5,9 +5,6 @@ if [ "$EUID" -eq 0 ]
   exit
 fi
 
-# bindl=~/.bin-dl
-# obsidian_version=1.3.7
-
 flatpak_apps=(
   org.mozilla.firefox
   com.nextcloud.desktopclient.nextcloud
@@ -41,24 +38,6 @@ ln -s nvim.appimage nvim
 ln -s nvim.appimage vim
 ln -s nvim.appimage vi
 cd ~
-
-# echo "[+] Installing Obsidian..."
-# mkdir -p $bindl
-# wget https://github.com/obsidianmd/obsidian-releases/releases/download/v$obsidian_version/Obsidian-$obsidian_version.AppImage \
-#   -O $bindl/obsidian.appimage
-# cd $bindl
-# chmod +x obsidian.appimage
-# ./obsidian.appimage --appimage-extract
-# cd squashfs-root
-# # replace Exec=AppRun --no-sandbox %U by the full path of the downloaded appimage
-# sed -i 's/^Exec=.*/Exec=\/home\/tb\/.bin-dl\/obsidian.appimage --no-sandbox %U/' obsidian.desktop
-# mkdir -p ~/.local/share/applications
-# mkdir -p ~/.local/share/icons/hicolor/512x512/apps
-# cp obsidian.desktop ~/.local/share/applications
-# cp obsidian.png ~/.local/share/icons/hicolor/512x512/apps
-# update-desktop-database ~/.local/share/applications -v
-# rm -rf ./squashfs-root
-# cd ~
 
 echo "[+] Installing dotfiles..."
 git clone https://github.com/tbalthazar/dotfiles ~/.dotfiles
