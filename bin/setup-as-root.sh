bc1eb407cec61ec97e04097f5e567562f5a41cc0
Thomas Balthazar <thomas@balthazar.info>
2024-02-16 16:16:04 +0100

    Don't install 1Password

--- bin/setup-as-root.sh
+++ bin/setup-as-root.sh
@@ -81,18 +81,6 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 /usr/sbin/groupadd docker 
 /usr/sbin/usermod -aG docker $username
 
-echo "[+] Installing 1Password..."
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-  gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' > /etc/apt/sources.list.d/1password.list
-mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-  tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-  gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-apt-get update && apt-get install -y 1password
-
 echo "[+] Installing Virtualbox..."
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $debian_codename contrib" > /etc/apt/sources.list.d/virtualbox.list
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | \
