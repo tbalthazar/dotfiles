e4a3a346f1f786cacd4de6f69e367049e5b18771
Thomas Balthazar <thomas@balthazar.info>
2024-02-29 15:25:20 +0100

    neovim: add autopairs plugin

--- dotfiles/config/nvim/lua/custom/plugins/init.lua
+++ dotfiles/config/nvim/lua/custom/plugins/init.lua
@@ -7,6 +7,18 @@ return {
     'christoomey/vim-tmux-navigator',
   },
   {
+    'windwp/nvim-autopairs',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require('nvim-tree').setup {
