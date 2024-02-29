9c07ce14610a61279336d46b50d7848d56c41cb5
Thomas Balthazar <thomas@balthazar.info>
2024-02-29 14:41:56 +0100

    neovim: add mapping to delete a buffer

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -346,11 +346,11 @@ require('lazy').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-d>'] = require('telescope.actions').delete_buffer },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
