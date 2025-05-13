adfb8246393534a556041fa43ea69af88d22788f
Thomas Balthazar <thomas@balthazar.info>
2025-05-13 18:32:16 +0200

    Add WSL-specific config (clipboard)

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -5,3 +5,9 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.lsp")
+
+-- WSL-specific setup
+in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+if in_wsl then
+  require("config.wsl")
+end
